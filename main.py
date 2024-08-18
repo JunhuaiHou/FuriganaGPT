aeb04592c4f59737af567ab098128ae6ed155342
junhuaihou <junhuai.hou@hotmail.com>
2024-08-18 20:46:59 +0100

    Reset main

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from modules.srt_loader import SRTLoader
 from modules.gpt_generator import GPTGenerator
-from modules.api.gpt_client import remove_brackets
-import re
+
 
 if __name__ == '__main__':
-    # raw_instruction = 'x'
-    # gpt_generator = GPTGenerator()
-    # srt_loader = SRTLoader(gpt_generator.gpt_name)
-    # if srt_loader.srt_file_path:
-    #     srt_text = srt_loader.load_srt()
-    #     print(f'Loaded subtitle file name {srt_loader.srt_file_name}')
-    #     responses = gpt_generator.get_responses(srt_text)
-    #     srt_loader.create_new_srt(responses)
-    # else:
-    #     print("No SRT file found in the current directory.")
-    print(remove_brackets("""（冥冥）明治神宮前(めいじじんぐうまえ)駅に
-渋谷と同様の“帳”が下りた"""))
+    raw_instruction = 'x'
+    gpt_generator = GPTGenerator()
+    srt_loader = SRTLoader(gpt_generator.gpt_name)
+    if srt_loader.srt_file_path:
+        srt_text = srt_loader.load_srt()
+        print(f'Loaded subtitle file name {srt_loader.srt_file_name}')
+        responses = gpt_generator.get_responses(srt_text)
+        srt_loader.create_new_srt(responses)
+    else:
+        print("No SRT file found in the current directory.")
