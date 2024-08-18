b1faabf6d36771e672a59c1928c2f402ad1e1a93
junhuaihou <junhuai.hou@hotmail.com>
2024-08-18 19:58:35 +0100

    Reset main

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from modules.srt_loader import SRTLoader
 from modules.gpt_generator import GPTGenerator
-from modules.api.gpt_client import remove_brackets
+
 
 if __name__ == '__main__':
-    # raw_instruction = 'x'
-    # gpt_generator = GPTGenerator()
-    # srt_loader = SRTLoader(gpt_generator.gpt_name)
-    # if srt_loader.srt_file_path:
-    #     srt_text = srt_loader.load_srt()
-    #     print(f'Loaded subtitle file name {srt_loader.srt_file_name}')
-    #     responses = gpt_generator.get_responses(srt_text)
-    #     srt_loader.create_new_srt(responses)
-    # else:
-    #     print("No SRT file found in the current directory.")
+    raw_instruction = 'x'
+    gpt_generator = GPTGenerator()
+    srt_loader = SRTLoader(gpt_generator.gpt_name)
+    if srt_loader.srt_file_path:
+        srt_text = srt_loader.load_srt()
+        print(f'Loaded subtitle file name {srt_loader.srt_file_name}')
+        responses = gpt_generator.get_responses(srt_text)
+        srt_loader.create_new_srt(responses)
+    else:
+        print("No SRT file found in the current directory.")
 
-    print(remove_brackets( "（メカ丸機内音声）術式装填(そうてん)" ))
 
