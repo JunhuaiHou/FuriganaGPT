99bd00e22e51385671ac3a34b2e1a3e5b63d4299
junhuaihou <junhuai.hou@hotmail.com>
2024-08-21 21:56:22 +0100

    Add moderation check

--- fine-tuner.py
+++ fine-tuner.py
@@ -5,10 +5,34 @@ from modules.data_builder import DataBuilder
 if __name__ == '__main__':
     gpt_client = GPTClient()
     data_builder = DataBuilder(gpt_client.instruction)
-    data_builder.save_training_data()
 
-    gpt_client.fine_tune(data_builder.training_data)
+    print("Moderation starting...")
 
-    data_builder.archive_training()
-    data_builder.rewrite_training_template()
+    compliance = True
 
+    for pair in data_builder.training_pairs:
+        prompt= pair[0]
+        moderation_result_0 = gpt_client.client.moderations.create(input=prompt)
+
+        if moderation_result_0.results[0].flagged:
+            print(prompt + " does violate rules!!!!!!!")
+            compliance = False
+            continue
+
+        answer = pair[1]
+        moderation_result_1 = gpt_client.client.moderations.create(input=answer)
+
+        if moderation_result_1.results[0].flagged:
+            print(answer + " does violate rules!!!!!!!")
+            compliance = False
+
+    if compliance:
+        print("Moderation complete. No OpenAI policy violation.")
+        data_builder.save_training_data()
+
+        gpt_client.fine_tune(data_builder.training_data)
+
+        data_builder.archive_training()
+        data_builder.rewrite_training_template()
+    else:
+        print("The training data violates OpenAI's policy!!!")
