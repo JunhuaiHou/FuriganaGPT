16db6631ca25c04c2beaeb76c7f2aed5533a874e
junhuaihou <junhuai.hou@hotmail.com>
2024-08-22 21:11:44 +0100

    Train after JJK 40

--- fine-tuner.py
+++ fine-tuner.py
@@ -1,5 +1,29 @@
 from modules.api.gpt_client import GPTClient
 from modules.data_builder import DataBuilder
+import threading
+
+
+def has_content(list_of_lists):
+    return any(sublist for sublist in list_of_lists)
+
+
+def moderate(pairs, results, index):
+    violations = []
+    for pair in pairs:
+        prompt = pair[0]
+        moderation_result_0 = gpt_client.client.moderations.create(input=prompt)
+
+        if moderation_result_0.results[0].flagged:
+            violations.append(prompt)
+            continue
+
+        answer = pair[1]
+        moderation_result_1 = gpt_client.client.moderations.create(input=answer)
+
+        if moderation_result_1.results[0].flagged:
+            violations.append(answer)
+
+    results[index] = violations
 
 
 if __name__ == '__main__':
@@ -10,29 +34,46 @@ if __name__ == '__main__':
 
     compliant = True
 
-    for pair in data_builder.training_pairs:
-        prompt = pair[0]
-        moderation_result_0 = gpt_client.client.moderations.create(input=prompt)
+    len_total_training_pairs = len(data_builder.training_pairs)
+    training_pairs = data_builder.training_pairs
+    for pair in training_pairs:
+        quarter_point = len_total_training_pairs // 4
+        results = [[] for _ in range(4)]
 
-        if moderation_result_0.results[0].flagged:
-            print(prompt + " does violate rules!!!!!!!")
-            compliant = False
-            continue
+        pairs_parts = [
+            training_pairs[:quarter_point],
+            training_pairs[quarter_point:quarter_point * 2],
+            training_pairs[quarter_point * 2:quarter_point * 3],
+            training_pairs[quarter_point * 3:]
+        ]
 
-        answer = pair[1]
-        moderation_result_1 = gpt_client.client.moderations.create(input=answer)
+        threads = []
 
-        if moderation_result_1.results[0].flagged:
-            print(answer + " does violate rules!!!!!!!")
-            compliant = False
+        for i in range(4):
+            thread = threading.Thread(
+                target=moderate,
+                args=(pairs_parts[i], results, i)
+            )
+            threads.append(thread)
+            thread.start()
 
-    if compliant:
+        for thread in threads:
+            thread.join()
+
+        violations = results[0] + results[1] + results[2] + results[3]
+
+    if has_content(violations):
+        print("The following training data violates OpenAI's policy!!!!!")
+        for item in violations:
+            print('------------------------------------')
+            print(item)
+
+    else:
         print("Moderation complete. No OpenAI policy violation.")
+
         data_builder.save_training_data()
 
         gpt_client.fine_tune(data_builder.training_data)
 
         data_builder.archive_training()
         data_builder.rewrite_training_template()
-    else:
-        print("The training data violates OpenAI's policy!!!")
